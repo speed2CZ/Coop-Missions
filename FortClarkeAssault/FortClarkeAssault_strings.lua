622fe36582652296957d32810962c1b4d9ff9af4
speed2CZ <gtgtgt@seznam.cz>
2016-03-01 11:14:52 +0100

    New Beta Mission Fort Clarke Assault

---
+++ FortClarkeAssault/FortClarkeAssault_strings.lua
@@ -0,0 +1,69 @@
+-- PO1 Factory West Destroyed / Actor:  / Update: 24/2/2016 / VO TODO
+BaseDestroyed = {
+  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+}
+
+
+------------
+-- Mission 2
+------------
+-- Assign objective to kill Civilian City / Actor:  / Update: 24/2/2016 / VO TODO
+M2_Kill_Civs_Objective = {
+  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+}
+
+-- Objective to Destroy M2 UEF City complete / Actor:  / Update: 24/2/2016 / VO TODO
+M2_Civs_Killed = {
+  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+}
+
+-- Order Commander Killed / Actor:  / Update: 24/2/2016 / VO TODO
+M2_OrderCommanderKilled = {
+  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+}
+
+
+------------
+-- Mission 3
+------------
+-- Experimentals from Counterattack killed / Actor:  / Update: 24/2/2016 / VO TODO
+M3_All_Exps_Killed = {
+  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+}
+
+
+
+------------
+-- Mission 4
+------------
+
+-- Ythothas and T2 subs for player / Actor:  / Update: 24/2/2016 / VO TODO
+M4_Reinforcements = {
+  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+}
+
+
+-- Fork Clarke Destroyed / Actor:  / Update: 24/2/2016 / VO TODO
+M4_Fort_Clarke_Destroyd = {
+  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+}
+
+-- Aeon ACU Killed / Actor:  / Update: 24/2/2016 / VO TODO
+M4_Aeon_ACU_Killed = {
+  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+}
+
+-- Cybran ACU Killed / Actor:  / Update: 24/2/2016 / VO TODO
+M4_Cybran_ACU_Killed = {
+  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+}
+
+-- UEF ACU Killed / Actor:  / Update: 24/2/2016 / VO TODO
+M4_UEF_ACU_Killed = {
+  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+}
+
+-- All ACUs Killed / Actor:  / Update: 24/2/2016 / VO TODO
+M4_All_ACUs_Killed = {
+  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+}
