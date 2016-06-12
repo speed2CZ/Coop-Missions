3d3310b01992a7b0ee87e4e9b55c5ef3961488c8
speed2CZ <gtgtgt@seznam.cz>
2016-06-12 23:54:20 +0200

    SeraMission1 - M1 update Order attacks

--- SeraMission1/SeraMission1_m1orderai.lua
+++ SeraMission1/SeraMission1_m1orderai.lua
@@ -1,4 +1,5 @@
 local ScenarioFramework = import('/lua/ScenarioFramework.lua')
+local ScenarioUtils = import('/lua/sim/ScenarioUtilities.lua')
 local ThisFile = '/maps/SeraMission1/SeraMission1_m1orderai.lua'
 
 ---------
@@ -7,6 +8,9 @@ local ThisFile = '/maps/SeraMission1/SeraMission1_m1orderai.lua'
 local Order = 3
 local Difficulty = ScenarioInfo.Options.Difficulty
 
+----------
+-- Carrier
+----------
 function OrderCarrierFactory()
     -- Adding build location for AI
 	ArmyBrains[Order]:PBMAddBuildLocation('M1_Order_Carrier_Start_Marker', 150, 'AircraftCarrier1')
@@ -31,6 +35,7 @@ function OrderCarrierFactory()
     end
 end
 
+-- Platoons built by carrier
 function OrderCarrierAttacks()
 	local torpBomberNum = {6, 4, 3}
     local swiftWindNum = {7, 5, 4}
@@ -52,77 +57,123 @@ function OrderCarrierAttacks()
         PlatoonType = 'Air',
         RequiresConstruction = true,
         LocationType = 'AircraftCarrier1',
-        PlatoonAIFunction = {ThisFile, 'GivePlatoonToPlayer'},       
+        PlatoonAIFunction = {ThisFile, 'GivePlatoonToPlayer'},
+        PlatoonData = {
+            PatrolChain = 'M1_Oder_Naval_Def_Chain',
+        },      
     }
     ArmyBrains[Order]:PBMAddPlatoon( Builder )
+end
 
-    --[[
-    quantity = {9, 7, 5}
-    Temp = {
-        'M1_Order_Carrier_Air_Attack_2',
-        'NoPlan',
-        { 'xaa0202', 1, quantity[Difficulty], 'Attack', 'AttackFormation' }, -- Swift Wind
-        { 'uaa0101', 1, 2, 'Attack', 'AttackFormation' }, -- T1 Scout
-    }
-    Builder = {
-        BuilderName = 'M1_Order_Carrier_Air_Builder_2',
-        PlatoonTemplate = Temp,
-        InstanceCount = 1,
-        Priority = 100,
-        PlatoonType = 'Air',
-        RequiresConstruction = true,
-        LocationType = 'AircraftCarrier1',
-        PlatoonAIFunction = {ThisFile, 'GivePlatoonToPlayer'},       
-    }
-    ArmyBrains[Order]:PBMAddPlatoon( Builder )
+----------
+-- Tempest
+----------
+function OrderTempestFactory()
+    ArmyBrains[Order]:PBMAddBuildLocation('M1_Order_Tempest_Start_Marker', 150, 'Tempest1')
 
-    quantity = {10, 8, 6}
-    Temp = {
-        'M1_Order_Carrier_Air_Attack_3',
-        'NoPlan',
-        { 'uaa0203', 1, quantity[Difficulty], 'Attack', 'AttackFormation' }, -- T2 Gunship
-        { 'uaa0101', 1, 2, 'Attack', 'AttackFormation' }, -- T1 Scout
-    }
-    Builder = {
-        BuilderName = 'M1_Order_Carrier_Air_Builder_3',
-        PlatoonTemplate = Temp,
-        InstanceCount = 1,
-        Priority = 100,
-        PlatoonType = 'Air',
-        RequiresConstruction = true,
-        LocationType = 'AircraftCarrier1',
-        PlatoonAIFunction = {ThisFile, 'GivePlatoonToPlayer'},       
-    }
-    ArmyBrains[Order]:PBMAddPlatoon( Builder )
-
-    if Difficulty <= 2 then
-        quantity = {2, 1, 0}
-        Temp = {
-            'M1_Order_Carrier_Air_Attack_4',
-            'NoPlan',
-            { 'xaa0305', 1, quantity[Difficulty], 'Attack', 'AttackFormation' }, -- T3 Gunship
-            { 'uaa0302', 1, 1, 'Attack', 'AttackFormation' }, -- T3 Scout
-        }
-        Builder = {
-            BuilderName = 'M1_Order_Carrier_Air_Builder_4',
-            PlatoonTemplate = Temp,
-            InstanceCount = 1,
-            Priority = 100,
-            PlatoonType = 'Air',
-            RequiresConstruction = true,
-            LocationType = 'AircraftCarrier1',
-            PlatoonAIFunction = {ThisFile, 'GivePlatoonToPlayer'},       
-        }
-        ArmyBrains[Order]:PBMAddPlatoon( Builder )
+    local Tempest = ScenarioInfo.M1_Order_Tempest
+    local location
+    for num, loc in ArmyBrains[Order].PBM.Locations do
+        if loc.LocationType == 'Tempest1' then
+            location = loc
+            OrderTempestAttacks()
+            break
+        end
     end
-    ]]--
+    location.PrimaryFactories.Sea = Tempest
+end
+
+function OrderTempestAttacks()
+    local destroyerNum = {3, 2, 1}
+    local frigateNum = {7, 5, 3}
+    local aaBoatNum = {8, 6, 2}
+
+    local Temp = {
+        'M1_Order_Tempest_Naval_Attack_1',
+        'NoPlan',
+        { 'uas0201', 1, 1, 'Attack', 'AttackFormation' },  -- Destroyer
+        { 'uas0103', 1, 3, 'Attack', 'AttackFormation' },  -- Frigate
+        { 'uas0102', 1, 1, 'Attack', 'AttackFormation' },  -- AA Boat
+    }
+    local Builder = {
+        BuilderName = 'M1_Order_Tempest_Naval_Builder_1',
+        PlatoonTemplate = Temp,
+        InstanceCount = 1,
+        Priority = 100,
+        PlatoonType = 'Sea',
+        RequiresConstruction = true,
+        LocationType = 'Tempest1',
+        PlatoonAIFunction = {ThisFile, 'MoveAndGivePlatoonToPlayer'},
+        PlatoonData = {
+            MoveRoute = {'Rally Point 05'},
+            PatrolChain = 'M1_Oder_Naval_Def_Chain',
+        },    
+    }
+    ArmyBrains[Order]:PBMAddPlatoon( Builder )
 end
 
 function GivePlatoonToPlayer(platoon)
+    local givenUnits = {}
+    local data = platoon.PlatoonData
+
 	for _, unit in platoon:GetPlatoonUnits() do
         while (not unit:IsDead() and unit:IsUnitState('Attached')) do
             WaitSeconds(1)
         end
-        ScenarioFramework.GiveUnitToArmy(unit, 'Player')
-    end    
+        local tempUnit = ScenarioFramework.GiveUnitToArmy(unit, 'Player')
+        table.insert(givenUnits, tempUnit)
+    end
+
+    if data.PatrolChain then
+        ScenarioFramework.GroupPatrolChain(givenUnits, data.PatrolChain)
+    end
+end
+
+function MoveAndGivePlatoonToPlayer(platoon)
+    local givenUnits = {}
+    local data = platoon.PlatoonData
+
+    if(data) then
+        if(data.MoveRoute or data.MoveChain) then
+            local movePositions = {}
+            if data.MoveChain then
+                movePositions = ScenarioUtils.ChainToPositions(data.MoveChain)
+            else
+                for k, v in data.MoveRoute do
+                    if type(v) == 'string' then
+                        table.insert(movePositions, ScenarioUtils.MarkerToPosition(v))
+                    else
+                        table.insert(movePositions, v)
+                    end
+                end
+            end
+            if(data.UseTransports) then
+                for k, v in movePositions do
+                    platoon:MoveToLocation(v, data.UseTransports)
+                end
+            else
+                for k, v in movePositions do
+                    platoon:MoveToLocation(v, false)
+                end
+            end
+        else
+            error('*SCENARIO PLATOON AI ERROR: MoveToRoute or MoveChain not defined', 2)
+        end
+    else
+        error('*SCENARIO PLATOON AI ERROR: PlatoonData not defined', 2)
+    end
+
+    WaitSeconds(1)
+
+    for _, unit in platoon:GetPlatoonUnits() do
+        while (not unit:IsDead() and unit:IsUnitState('Moving')) do
+            WaitSeconds(1)
+        end
+        local tempUnit = ScenarioFramework.GiveUnitToArmy(unit, 'Player')
+        table.insert(givenUnits, tempUnit)
+    end
+
+    if data.PatrolChain then
+        ScenarioFramework.GroupPatrolChain(givenUnits, data.PatrolChain)
+    end
 end
