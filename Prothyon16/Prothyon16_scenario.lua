4dda9106ab53c30f288f9bc684201962d5a572b5
speed2CZ <GTGTGT@seznam.cz>
2016-10-04 09:43:18 +0200

    Update Prothyon
    Morax should really leave his base now
    Clean up of the code

--- Prothyon16/Prothyon16_scenario.lua
+++ Prothyon16/Prothyon16_scenario.lua
@@ -1,12 +1,12 @@
 version = 3
 ScenarioInfo = {
-    name = 'Prothyon - 16',
-    description = '',
+    name = 'Prothyon - 16 Beta',
+    description = "Prothyon - 16 is a secret UEF facillity for training new ACU pilots. Your task today is to provide a demonstration to our newest recruits by fighting against a training AI.",
     type = 'campaign_coop',
     starts = true,
     preview = '',
     size = {2048, 2048},
-    map = '/maps/X1MP_010/X1MP_010.scmap',
+    map = '/maps/Prothyon16/Prothyon16.scmap',
     save = '/maps/Prothyon16/Prothyon16_save.lua',
     script = '/maps/Prothyon16/Prothyon16_script.lua',
     norushradius = 0.000000,
